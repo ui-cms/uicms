a5e125e53c604695531869f442a2ed4b4d350786
Elnur Mammadli <elnur@elforte.ca>
2023-02-15 01:41:57 -0700

    placeholder image fixed

--- components/layout.js
+++ components/layout.js
@@ -199,7 +199,7 @@ function Header() {
                     <figure className="image is-24x24 is-align-self-center">
                       <Image
                         className="is-rounded"
-                        src="https://bulma.io/images/placeholders/128x128.png"
+                        src="/user.png"
                         width="32"
                         height="32"
                         alt="username"
