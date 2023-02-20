356187f2d75058bf6486cb22717b5ef2de4d9998
Elnur Mammadli <elnur@elforte.ca>
2023-02-20 01:51:25 -0700

    usestorage hook introduced

---
+++ hooks/useStorage.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+/**
+ * This React hook will allow to use browser storages (sessionStorage or localStorage).
+ * Next.js performs a server render before the client render, therefore as default it will complain that storage is not defined.
+ * As useEffect hook only runs in client side, it is guaranteed that storages will be defined within it.
+ * @param {string} key the key of the item to set in storage
+ * @param {string} type storage type: sessionStorage (default) or localStorage
+ * @returns
+ */
+export default function useStorage(key, type = "sessionStorage") {
+  const [value, setValue] = useState();
+
+  // Initial fetch from storage
+  useEffect(() => {
+    const storage = type === "sessionStorage" ? sessionStorage : localStorage;
+    setValue(storage.getItem(key));
+  }, [key, type]);
+
+  // Persist to storage
+  useEffect(() => {
+    // first render, don't override/destroy existing item value
+    if (value !== undefined) {
+      const storage = type === "sessionStorage" ? sessionStorage : localStorage;
+      storage.setItem(key, value);
+    }
+  }, [key, value, type]);
+
+  return [value, setValue];
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from "@/styles/Home.module.css";
 import useStateManagement from "@/services/stateManagement/stateManagement";
 import { useRouter } from "next/router";
 import Page from "@/components/page";
+import useStorage from "@/hooks/useStorage";
 
 /**
  * This page is accessible only when not authenticated. Otherwise will redirect to /repos
@@ -10,15 +11,23 @@ export default function Home() {
   const { state } = useStateManagement();
   const { currentUser } = state;
   const router = useRouter();
-
+  const [testVal, setTestVal] = useStorage("test");
+  
   if (currentUser) {
-    router.push("/repos");  // redirect to repos page when authorized user
+    router.push("/repos"); // redirect to repos page when authorized user
     return null;
   }
   return (
     <Page authProtected={false}>
       <h1>Home</h1>
       <p>Sign in using yout GitHub account</p>
+      {testVal && (
+        <p>
+          {testVal} 
+        </p>
+      )}<button onClick={() => setTestVal(new Date().getTime())}>start</button>
     </Page>
   );
 }
+
+
