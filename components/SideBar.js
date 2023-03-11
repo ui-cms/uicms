a4bafa61a65c9ad271717b9c13f5760e8dde9342
Elnur Mammadli <elnur@elforte.ca>
2023-03-11 06:17:22 -0700

    close icon when mobile menu open

--- components/SideBar.js
+++ components/SideBar.js
@@ -14,7 +14,7 @@ export default function SideBar({}) {
     <Suspense fallback={<Loading />}>
       <aside className={styles.sidebar}>
         <nav className={styles.header}>
-          <i onClick={() => setOpen(!open)}>☰</i>
+          <i onClick={() => setOpen(!open)}>{open ? "✕" : "☰"}</i>
           <span className={styles.brand}>UI CMS</span>
 
           {currentUser && (
