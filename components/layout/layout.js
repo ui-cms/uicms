0f0c18b29787ce690100c026f7dfee960ed1fdb4
Elnur Mammadli <elnur@elforte.ca>
2023-03-13 16:21:20 -0600

    meta theme color

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#f0f4f8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${inter.className} ${styles.layout}`}>
