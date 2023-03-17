e483abba7a67f1988e0c422c37953636e4cb14b7
Elnur Mammadli <elnur@elforte.ca>
2023-03-17 05:41:06 -0600

    test page fixed

--- pages/test.js
+++ pages/test.js
@@ -5,44 +5,44 @@ import useStorage from "@/hooks/useStorage";
 export default function Test() {
   const [testVal, setTestVal] = useStorage("test", "localStorage");
   return (
-    <Page authProtected={false}>
+    <Page authProtected={true}>
       <div className="p-4">
         <h1>Test page</h1>
-        {testVal && <p>{testVal}</p>}
-        <p>
+        {testVal && <div>{testVal}</div>}
+        <div>
           <button onClick={() => setTestVal(new Date().getTime())}>
             Set time in localStorage
           </button>
-        </p>
+        </div>
 
-        <p>
+        <div>
           <button
             className="primary"
             onClick={() => setTestVal(new Date().getTime())}
           >
             Primary
           </button>
-        </p>
+        </div>
 
-        <p>
+        <div>
           <button
             className="primary light"
             onClick={() => setTestVal(new Date().getTime())}
           >
             Primary
           </button>
-        </p>
+        </div>
 
-        <p>
+        <div>
           <input placeholder="Enter name here" type="text" />
-        </p>
-        <p>
+        </div>
+        <div>
           <label>
             <input placeholder="Enter name here" type="checkbox" />
             check
           </label>
           <Loader />
-        </p>
+        </div>
       </div>
     </Page>
   );
