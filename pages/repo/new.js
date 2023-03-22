0f97f5cb4455e59853c5e09af80a9b44c564ca01
Elnur Mammadli <elnur@elforte.ca>
2023-03-22 02:55:14 -0600

    new repo componenent added

--- pages/repo/new.js
+++ pages/repo/new.js
@@ -1,1 +1,9 @@
-// new repo creation
+import Page from "@/components/page";
+
+export default function NewRepo() {
+  return (
+    <Page title="New repo">
+      <h1>New repo</h1>
+    </Page>
+  );
+}
